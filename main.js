a12ec530a63980f91e15c85bac508a67925cfd04
Wendel Nogueira <wnogueira197@gmail.com>
2021-08-11 21:37:24 -0300

    :sparkles: Add footer and features

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ for (const link of links){
 
 /*    =====    Header Shadow    =====    */
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function headerScroll(){
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function(){
     if (window.scrollY >= navHeight){
         header.classList.add('scroll')
     } else{
         header.classList.remove('scroll')
     }
-})
+}
 
 /*    =====    Swiper    =====    */
 
@@ -54,5 +54,25 @@ scrollReview.reveal(`
     #about .image, #about .text,
     #services header, #services .card,
     #testimonials header, #testimonials .testimonials,
-    #contact .text, #contact .links
+    #contact .text, #contact .links,
+    footer .brand, footer .social
 `, { interval: 100 })
+
+/*    =====    Back to top    =====    */
+
+function backToTop(){
+    const backToTop = document.querySelector('.back-to-top')
+
+    if (window.scrollY >= 560){
+        backToTop.classList.add('show')
+    } else{
+        backToTop.classList.remove('show')
+    }
+}
+
+/*    =====    Scroll Event    =====    */
+
+window.addEventListener('scroll', function(){
+    headerScroll()
+    backToTop()
+})
